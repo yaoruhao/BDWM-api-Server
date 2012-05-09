9679514cd7ed840146e310d73613d95719bf6a7c
Ruhao Yao <yaoruhao@gmail.com>
2012-05-09 20:35:19 +0800

    Add school and academic hot topics, use regex instead of string search

--- src/net/bdwm/api/model/HotTopicsModel.java
+++ src/net/bdwm/api/model/HotTopicsModel.java
@@ -4,12 +4,34 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class HotTopicsModel {
-	
-	//Top 10 topics
+
+	// Top 10 topics
 	private ArrayList<Topic> allTopTopics;
-	
-	//Each divison top topics
+
+	// Each divison top topics
 	private HashMap<String, ArrayList<Topic>> divisionTopTopics;
+
+	// School hot topics
+	private ArrayList<Topic> schoolHotTopics;
+
+	// Academic hot topics
+	private ArrayList<Topic> academicHotTopics;
+
+	public ArrayList<Topic> getAcademicHotTopics() {
+		return academicHotTopics;
+	}
+
+	public void setAcademicHotTopics(ArrayList<Topic> academicHotTopics) {
+		this.academicHotTopics = academicHotTopics;
+	}
+
+	public ArrayList<Topic> getSchoolHotTopics() {
+		return schoolHotTopics;
+	}
+
+	public void setSchoolHotTopics(ArrayList<Topic> schoolHotTopics) {
+		this.schoolHotTopics = schoolHotTopics;
+	}
 
 	public ArrayList<Topic> getAllTopTopics() {
 		return allTopTopics;
@@ -27,10 +49,12 @@ public class HotTopicsModel {
 			HashMap<String, ArrayList<Topic>> divisionTopTopics) {
 		this.divisionTopTopics = divisionTopTopics;
 	}
-	
-	public HotTopicsModel(){
+
+	public HotTopicsModel() {
 		allTopTopics = new ArrayList<Topic>();
 		divisionTopTopics = new HashMap<String, ArrayList<Topic>>();
+		schoolHotTopics = new ArrayList<Topic>();
+		academicHotTopics = new ArrayList<Topic>();
 	}
 
 }
