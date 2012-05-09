4744ab4326a3396f3ca04df2ba2d93d3d15b9398
Ruhao Yao <yaoruhao@gmail.com>
2012-05-09 12:19:42 +0800

    parse main page added

--- src/net/bdwm/api/utils/HotTopicsManager.java
+++ src/net/bdwm/api/utils/HotTopicsManager.java
@@ -3,10 +3,13 @@ package net.bdwm.api.utils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.bdwm.api.model.HotTopicsModel;
+import net.bdwm.api.model.Topic;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
@@ -25,6 +28,14 @@ public class HotTopicsManager implements Runnable{
 	
 	private String topTenPatternStr="<span id=\"DefaultTopTen\">.+<span id='TopTenMoreButton' class='More'>";
 	
+	private String topTenPatternEachStr="<span class='hidden'>.*?</a><br /></span>";
+	
+	private String divisionPatternStr="·ÖÇø£º";
+	
+	private Pattern divisionPattern;
+	
+	private Pattern topTenEachPattern;
+	
 	private Pattern topTenPattern;
 	
 	public String getBbsPrefixUrl() {
@@ -41,6 +52,8 @@ public class HotTopicsManager implements Runnable{
 		}
 		logger.info("HotTopicsManager init succeed.");
 		topTenPattern = Pattern.compile(topTenPatternStr);
+		topTenEachPattern = Pattern.compile(topTenPatternEachStr);
+		divisionPattern = Pattern.compile(divisionPatternStr);
 	}
 	
 	public static synchronized HotTopicsManager getInstance(){
@@ -85,9 +98,74 @@ public class HotTopicsManager implements Runnable{
 	
 	private void parseMainBoard(String resource) {
 		Matcher m = topTenPattern.matcher(resource);
+		ArrayList<Topic> topTenTopics = new ArrayList<Topic>();
 		if (m.find()){
-			logger.info(m.group());
+			String topTenStr = m.group();
+			parseTopTenTopics(topTenTopics, topTenStr);		
 		}
+		m = divisionPattern.matcher(resource);
+		int startIndex = 0;
+		HashMap<String, ArrayList<Topic>> divisionTopicsHashMap = new HashMap<String, ArrayList<Topic>>();
+		while (resource.indexOf(divisionPatternStr, startIndex) != -1){
+			startIndex = resource.indexOf(divisionPatternStr, startIndex) + divisionPatternStr.length();
+			int endIndex = resource.indexOf("</td>", startIndex);
+			String tempStr = resource.substring(startIndex, endIndex);
+			startIndex = endIndex;
+			parseDivisionTopics(divisionTopicsHashMap, tempStr);
+		}
+		System.out.println(divisionTopicsHashMap);
+		System.out.println(topTenTopics);
+		
+	}
+	
+	private void parseDivisionTopics(HashMap<String, ArrayList<Topic>> divisionTopics, String dataStr) {
+		
+		String topDivision = dataStr.substring(dataStr.indexOf(">") + 1, dataStr.indexOf("<", dataStr.indexOf(">")));
+		ArrayList<Topic> topicList = new ArrayList<Topic>();
+		int startIndex = dataStr.indexOf("[<a href='bbsdoc.php?board=");
+		while (startIndex != -1) {
+			startIndex = dataStr.indexOf(">", startIndex) + ">".length();
+			int endIndex = dataStr.indexOf("<", startIndex);
+			String division = dataStr.substring(startIndex, endIndex);
+			startIndex = dataStr.indexOf("<a href='", endIndex) + "<a href='".length();
+			endIndex = dataStr.indexOf("'>", startIndex);
+			String url = dataStr.substring(startIndex, endIndex);
+			String threadId = url.substring(url.indexOf("threadid=") + "threadid=".length());
+			String board = url.substring(url.indexOf("board=") + "board=".length(), url.indexOf("&"));
+			startIndex = endIndex + "'>".length();
+			endIndex = dataStr.indexOf("</a>", startIndex);
+			String name = dataStr.substring(startIndex, endIndex);
+			startIndex = dataStr.indexOf("[<a href='bbsdoc.php?board=", endIndex);
+			Topic topic = new Topic(name, division, board, threadId, url);
+			topicList.add(topic);
+			
+		}
+		divisionTopics.put(topDivision, topicList);
+		
+		
+	}
+	
+	private void parseTopTenTopics(ArrayList<Topic> topTenTopics, String topTenStr) {		
+		Matcher topTenMatch = topTenEachPattern.matcher(topTenStr);
+		while (topTenMatch.find()) {
+			String matchStr = topTenMatch.group();
+			String tempStr = "bbsdoc.php?board=";
+			int startIndex = matchStr.indexOf(tempStr) + tempStr.length();
+			int endIndex = matchStr.indexOf("'", startIndex);
+			String board = matchStr.substring(startIndex, endIndex);
+			startIndex = endIndex + ".>".length();
+			endIndex = matchStr.indexOf("<", startIndex);
+			String division = matchStr.substring(startIndex, endIndex);
+			startIndex = matchStr.indexOf("href='", endIndex) + "href='".length();
+			endIndex = matchStr.indexOf("'>", startIndex);
+			String url = matchStr.substring(startIndex, endIndex);
+			String threadId = url.substring(url.indexOf("threadid=") + "threadid=".length());
+			startIndex = endIndex + ".>".length();
+			endIndex = matchStr.indexOf("</a>", startIndex);
+			String topicName = matchStr.substring(startIndex, endIndex);
+			Topic topic = new Topic(topicName, division, board, threadId, url);
+			topTenTopics.add(topic);				
+		}	
 		
 	}
 
